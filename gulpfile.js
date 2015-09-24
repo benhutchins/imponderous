08f593e2a42113eb465c2b60d1b46ada79566995
Benjamin Hutchins <ben@hutchins.co>
2015-09-24 17:02:56 -0400

    New gulp task to lint using standardjs

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,16 @@
 
 let gulp = require('gulp')
 let mocha = require('gulp-mocha')
+let standard = require('gulp-standard')
 var del = require('del')
+
+gulp.task('lint', function () {
+  return gulp.src(['index.js', 'lib/**/*.js', 'test/**/*.js'])
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true
+    }))
+})
 
 gulp.task('clean:test', function () {
   return del([
@@ -10,7 +19,7 @@ gulp.task('clean:test', function () {
   ])
 })
 
-gulp.task('test', ['clean:test'], function () {
+gulp.task('test', ['clean:test', 'lint'], function () {
   return gulp.src('test/*.js', { read: false })
     .pipe(mocha())
 })
