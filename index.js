3176f0d8b99515dbce2980ace6f5ce064b256530
Benjamin Hutchins <ben@hutchins.co>
2015-09-29 00:38:10 -0400

    New singleton class allows you to have a collection which acts like a singleton (only one item).

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 let imponderous = require('./lib/')
+
 imponderous.Model = require('./lib/model')
+imponderous.SingletonModel = require('./lib/singleton-model')
 
 module.exports = imponderous
